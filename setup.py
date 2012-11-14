f6d071bf695d6cde529ada63fd2153c6c0ed214e
Alexander Saltanov <asd@mokote.com>
2012-11-14 20:34:10 +0400

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='Flask-GoogleAuth',
-    version='0.1',
+    version='0.2',
     url='https://github.com/sashka/flask-googleauth',
     license='BSD',
     author='Alexander Saltanov',
